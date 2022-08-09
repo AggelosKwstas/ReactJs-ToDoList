72efc06638816f1c00a6d8769e8ded1a9d6efd11
AggelosKwstas <agelos.kostas21@gmail.com>
2022-08-09 10:37:26 +0300

    fixed user

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, {useState, useRef, useEffect} from 'react';
 import TodoList from './TodoList'
-import { v4 as uuidv4 } from 'uuid'
+import {v4 as uuidv4} from 'uuid'
 
 
 const LOCAL_STORAGE_KEY = 'todoApp.todos'
@@ -29,7 +29,7 @@ function App() {
         const name = todoNameRef.current.value
         if (name === '') return
         setTodos(prevTodos => {
-            return [...prevTodos, { id: uuidv4(), name: name, complete: false}]
+            return [...prevTodos, {id: uuidv4(), name: name, complete: false}]
         })
         todoNameRef.current.value = null
     }
@@ -46,8 +46,8 @@ function App() {
 
     return (
         <>
-            <TodoList todos={todos} toggleTodo={toggleTodo} />
-            <input ref={todoNameRef} type="text" />
+            <TodoList todos={todos} toggleTodo={toggleTodo}/>
+            <input ref={todoNameRef} type="text"/>
             <button onClick={handleAddTodo}>Add Todo</button>
             <button onClick={handleClearTodos}>Clear Complete</button>
             <div>{todos.filter(todo => !todo.complete).length} left to do</div>
